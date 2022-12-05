d61d8450848dc4a0ee9e86a4318988a5c6188774
baraa65 <baraash65@hotmail.com>
2022-12-05 21:45:44 +0200

    replace filter inputs

--- src/components/search-form.vue
+++ src/components/search-form.vue
@@ -5,30 +5,22 @@
 				<div class="q-pb-sm">
 					<div class="q-pb-sm">
 						<div class="q-pb-xs">رقم الاذن التسلسلي</div>
-						<q-input v-model="filter.billNumber" dense outlined style="width: 250px" />
+						<Field v-model="filter.billNumber" style="width: 250px" />
 					</div>
 					<div class="q-pb-sm">
 						<div class="q-pb-xs">تاريخ</div>
-						<q-input
-							dense
-							outlined
-							v-model="filter.billDate"
-							mask="date"
-							:rules="['date']"
-							style="width: 250px"
-						>
-							<template v-slot:append>
-								<q-icon color="primary" name="event" class="cursor-pointer">
-									<q-popup-proxy cover transition-show="scale" transition-hide="scale">
-										<q-date v-model="filter.billDate">
-											<div class="row items-center justify-end">
-												<q-btn v-close-popup label="Close" color="primary" flat />
-											</div>
-										</q-date>
-									</q-popup-proxy>
-								</q-icon>
-							</template>
-						</q-input>
+						<div class="date-field">
+							<Field v-model="filter.billDate" name="validityDate" style="flex: 1" />
+							<q-icon color="primary" name="event" class="cursor-pointer">
+								<q-popup-proxy cover transition-show="scale" transition-hide="scale">
+									<q-date v-model="filter.billDate" mask="DD-MM-YYYY">
+										<div class="row items-center justify-end">
+											<q-btn v-close-popup label="Close" color="primary" flat />
+										</div>
+									</q-date>
+								</q-popup-proxy>
+							</q-icon>
+						</div>
 					</div>
 				</div>
 
@@ -56,6 +48,7 @@
 </template>
 
 <script>
+import { Field } from 'vee-validate'
 import { defineComponent } from 'vue'
 
 const filter = () => ({ billNumber: '', billDate: null })
@@ -71,5 +64,6 @@ export default defineComponent({
 			this.$emit('filter-change', this.filter)
 		},
 	},
+	components: { Field },
 })
 </script>
